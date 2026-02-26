d3f33c996bed4be3f7bd35cd602aa46844b6b68e
Christoph Heil <christoph.heil156@gmail.com>
2026-02-26 17:52:56 +0100

    Update requirements and add config management with Pydantic

---
+++ empty/src/config.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+from logging import getLogger
+from packaging.version import Version
+from typing import Any
+
+import json
+from pydantic import BaseModel, Field, field_validator, ValidationError
+
+
+VERSION = Version("1.2.0dev1")
+logger = getLogger(__name__)
+DICTDEFAULTS: dict[str, dict[Any, Any]] = {}
+CONFIGFILE = Path.cwd() / "config.json"
+
+
+class Config(BaseModel):
+    pass
+
+
+    @staticmethod
+    def _checkKeys(d: dict, defaults: dict) -> None:
+        invalidKeys: list[str] = []
+        missingKeys: list[str] = []
+        for key in d.keys():
+            if key not in defaults:
+                invalidKeys.append(key)
+        for defaultKey in defaults.keys():
+            if defaultKey not in d:
+                missingKeys.append(defaultKey)
+        if len(invalidKeys) > 0:
+            logger.warning(f"Invalid keys in config: {', '.join(invalidKeys)}")
+        if len(missingKeys) > 0:
+            logger.info(f"Missing keys in config: {', '.join(missingKeys)}")
+        for missingKey in missingKeys:
+            d[missingKey] = defaults[missingKey]
+        for invalidKey in invalidKeys:
+            del d[invalidKey]
+
+
+    def save(self) -> None:
+        if CONFIGFILE.exists() and not CONFIGFILE.is_file():
+            raise RuntimeError("config.json is not a file! Aborting.")
+        try:
+            with open(CONFIGFILE, "w", encoding="utf-8") as f:
+                json.dump(self.model_dump(), f, ensure_ascii=False, indent=4)
+        except PermissionError as e:
+            logger.error(f"Permission error saving config")
+            raise
+
+
+    @classmethod
+    def load(cls, skipCreate: bool = False) -> "Config":
+        if CONFIGFILE.exists() and not CONFIGFILE.is_file():
+            raise RuntimeError("config.json is not a file! Aborting.")
+        if not CONFIGFILE.exists():
+            logger.warning("config.json does not exist!")
+            if not skipCreate:
+                logger.info("Creating default config.json")
+                return cls.createDefault()
+        try:
+            with open(CONFIGFILE, "r", encoding="utf-8") as f:
+                data = json.load(f)
+        except json.JSONDecodeError as e:
+            logger.error(f"Error parsing config.json: {e}")
+            if not skipCreate:
+                logger.info(f"Creatiing a backup of the invalid {CONFIGFILE.name} as {CONFIGFILE.name}.bak")
+                return cls.createDefault()
+            else:
+                raise
+        else:
+            logger.info("Config loaded successfully")
+            config = cls(**data)
+            config.save()
+            return config
+
+
+    @classmethod
+    def createDefault(cls) -> "Config":
+        config = cls()
+        config.save()
+        return config
+
+
+CONFIG = Config.load()
